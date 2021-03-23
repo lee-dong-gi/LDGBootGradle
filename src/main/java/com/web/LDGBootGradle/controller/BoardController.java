815e074b7162544d21c02a65363d7e019789b0d1
leedonggi <oksk327@naver.com>
2021-03-23 23:45:20 +0900

    게시판 view 및 view에 이미지 출력 추가

--- src/main/java/com/web/LDGBootGradle/controller/BoardController.java
+++ src/main/java/com/web/LDGBootGradle/controller/BoardController.java
@@ -7,22 +7,27 @@ import com.web.LDGBootGradle.repository.FileRepository;
 import com.web.LDGBootGradle.service.BoardService;
 import com.web.LDGBootGradle.service.FileService;
 import com.web.LDGBootGradle.validator.BoardValidator;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -77,6 +82,39 @@ public class BoardController {
         return "board/list";
     }
 
+    @GetMapping("/view")
+    public String view(Model model, @RequestParam(required = false) Long id, HttpServletRequest request){
+        if(id==null) {
+            model.addAttribute("board", new Board());
+        }else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board", board);
+            if (!("").equals(board.getUploadFileId()) & board.getUploadFileId()!=null){
+                String basicpath = request.getRequestURL().toString().replace(request.getRequestURI(),"");
+                String filepath = basicpath + "/download/board/" + board.getUploadFileId();
+
+                UploadFile uploadFile = fileService.getFile(board.getUploadFileId());
+                String imagepath = "/uploads/";
+                String fileDownLoadUri =  uploadFile.getFileDownloadUri();
+                String fileType = uploadFile.getFileType();
+
+                String[] strarr;
+                strarr = fileDownLoadUri.split(imagepath);
+                fileDownLoadUri = strarr[1];
+
+                imagepath = imagepath + fileDownLoadUri;
+
+                System.out.println("imagepath ::::: "+imagepath);
+
+                model.addAttribute("filepath",filepath);
+                model.addAttribute("imagepath",imagepath);
+                model.addAttribute("filename",uploadFile.getFileName());
+                //<img src="http://www.나의도메인.com/images/MyImg.jpg" alt="이미지" />
+            }
+        }
+        return "board/view";
+    }
+
     @GetMapping("/form")
     public String form(Model model, @RequestParam(required = false) Long id){
         if(id==null) {
@@ -122,13 +160,29 @@ public class BoardController {
         System.out.println("fileupload start");
         List<UploadFile> uploadFileList = new ArrayList<UploadFile>();
         try {
-            String modifileName = UUID.randomUUID().toString();
-            String baseDir = "D:/intelij/spring/LDGBootGradle/uploads";
-            String filePath = baseDir + "/" + modifileName;
-            files.transferTo(new File(filePath));
+
+            String baseDir = "D:/intelij/spring/LDGBootGradle/src/main/resources/static/uploads";
+
+
             Long size = files.getSize();
             String fileType = files.getContentType();
             String fileName = files.getOriginalFilename();
+
+            String[] fileTypeArr;
+            String extention;
+            fileTypeArr = fileType.split("/");
+
+            if(fileTypeArr[1].equals("jpeg")){
+                extention = "jpg";
+            }else{
+                extention = fileTypeArr[1];
+            }
+
+            String modifileName = UUID.randomUUID().toString() + "." +extention;
+
+            String filePath = baseDir + "/" + modifileName;
+
+            files.transferTo(new File(filePath));
 
             System.out.println(modifileName);
             System.out.println(filePath);
@@ -151,4 +205,6 @@ public class BoardController {
             return uploadFileList;
         }
     }
+
+
 }
